5e86e77f7cdb91540c86c970d16ff31b11219b00
Anachronox <fayiron@outlook.de>
2023-04-17 15:35:22 +0200

    nvm

--- main_llama-cpp-py.py
+++ main_llama-cpp-py.py
@@ -14,9 +14,6 @@ import requests
 # now the necessary imports for llama-cpp-python
 from llama_cpp import Llama
 
-# flag to check if alpaca13b is in use
-alpaca13b_in_use = False
-
 # necessary discord stuff
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -32,20 +29,12 @@ async def alpaca13b(interaction, prompt: str):
     print(prompt)
     ogprompt = prompt
     
-    # check if alpaca13b is in use
-    if (alpaca13b_in_use):
-        # if it is send an ephemeral message to the user, saying that it's in use and return
-        await interaction.response.send_message("alpaca13b is in use! Try again later.", ephemeral=True)
-        return
-    
     # check if the channel id is 768600365602963496
     if interaction.channel_id == 768600365602963496:
         # if it is send an ephemeral message to the user, saying that they can't use this command in this channel and return
         await interaction.response.send_message("You can't use this command in this channel! Move to #bot-fun instead.", ephemeral=True)
         return
     
-    # set alpaca13b_in_use to true
-    alpaca13b_in_use = True
     # get the prompt into the right format (Q: prompt A: )
     prompt = "Q: " + prompt + " A: "
     await interaction.response.send_message("Your prompt: " + ogprompt)
@@ -53,8 +42,6 @@ async def alpaca13b(interaction, prompt: str):
     output = llm(prompt, max_tokens=450, stop=["Q: ", "\n"], echo=True)
     # the output is in json format so we need to extract the text, which is in the text key under choices and everything after A:
     await interaction.followup.send(output["choices"][0]["text"].split("A: ")[1])
-    # set alpaca13b_in_use to false
-    alpaca13b_in_use = False
 
 @tree.command(name = "pyping", description = "Responds with Pong")
 async def ping(interaction):
